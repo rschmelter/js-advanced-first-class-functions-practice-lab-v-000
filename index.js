a609bca3aa0b2d20132c46ca98c40c2d9ca714b0
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-14 00:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ function logDriversByHometown(drivers, location) {
 const driversByRevenue = function (drivers) {
   return drivers.slice().sort(function (driverOne, driverTwo) {
     return driverOne.revenue - driverTwo.revenue;
-  });
-};
+  })
+}
 
 const driversByName = function (drivers) {
   return drivers.slice().sort(function (driverOne, driverTwo) {
     return driverOne.name.localeCompare(driverTwo.name);
-  });
+  })
+}
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0)
+}
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
 };
